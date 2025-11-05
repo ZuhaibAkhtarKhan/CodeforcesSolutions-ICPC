b81b5400b8199b78a584637788663ce293a0888a
Zuhaib Akhtar <zuhaibakhtarkhan@gmail.com>
2025-11-05 23:38:50 +0500

    Added 144A.cpp

--- 144A.cpp
+++ 144A.cpp
@@ -4,6 +4,28 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    vector <int> heights(n);
+
+    vector <int> h(n);
+
+    for(int i = 0; i < n; i++) {
+        cin >> h[i];
+    }
+
+    int max = 0;
+    int min = 0;
+
+    for(int i = 1; i < n; i++) {
+        if(h[max] < h[i]) max = i;
+        if(h[min] >= h[i]) min = i;
+    }
+    
+    if(max > min) {
+        cout << (n - 1 - min) + max - 1;
+    }
+    else if(max < min) {
+        cout << max + (n - 1 - min);
+    };
+
+    
     
 }
