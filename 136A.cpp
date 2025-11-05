defcc29e09d38ed4d321c6eab49da6d87fb7322e
Zuhaib Akhtar <zuhaibakhtarkhan@gmail.com>
2025-11-05 23:38:50 +0500

    Added 136A.cpp

--- 136A.cpp
+++ 136A.cpp
@@ -5,5 +5,16 @@ int main() {
     int n;
     cin >> n;
 
+    vector <int> givesTo(n+1);
+    vector <int> receivesFrom(n+1);
+
+    for(int i = 1; i <= n; i++) {
+        cin >> givesTo[i];
+        receivesFrom[givesTo[i]] = i;
+    }
+
     
+    for(int i = 1; i<= n; i++) {
+        cout << receivesFrom[i] << " ";
+    }
 }
