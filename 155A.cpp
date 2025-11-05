c65aeeccb2723007e3af00472a97ea1d01072cee
Zuhaib Akhtar <zuhaibakhtarkhan@gmail.com>
2025-11-06 02:53:45 +0500

    Added 155A.cpp

--- 155A.cpp
+++ 155A.cpp
@@ -2,5 +2,25 @@
 using namespace std;
 
 int main() {
-    
+    int n, inp, count = 0;
+    cin >> n;
+
+    int max;
+    cin >> max;
+    int min = max;
+
+    for(int i = 1; i< n; i++) {
+        cin >> inp;
+        if(max < inp) {
+            max = inp;
+            count++;
+        } 
+        if(min > inp) {
+            min = inp;
+            count++;
+        }
+    }
+    cout << count;
+
+
 }
