fb1e9a65bb7f7587d4491ad9ecdf51610854a3e6
Zuhaib Akhtar <zuhaibakhtarkhan@gmail.com>
2025-11-06 22:06:00 +0500

    Added 1899A.cpp

---
+++ 1899A.cpp
@@ -0,0 +1,15 @@
+#include <bits/sdtc++.h>
+using namespace std;
+
+int main() {
+    short int t, n, turns = 0;
+    cin >> t;
+
+    while(t--) {
+        cin >> n;
+        bool vaTurn = true;
+        while(n%3 != 0 && turns <= 10) {
+            if()
+        }
+    }
+}
