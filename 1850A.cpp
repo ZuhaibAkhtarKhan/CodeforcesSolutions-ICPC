5dafbe3da2634cbef5dfda72b50b89dd1c4d84e5
Zuhaib Akhtar <zuhaibakhtarkhan@gmail.com>
2025-11-06 22:05:59 +0500

    Added 1850A.cpp

---
+++ 1850A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t, a, b, c;
+    cin >> t;
+
+    while(t--) {
+        cin >> a >> b >> c;
+        if(a + b >= 10 || a + c >=10 || b+c >= 10) cout << "YES\n";
+        else cout << "NO\n";
+    }
+}
