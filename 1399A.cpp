c7510511a440a55974cb99e13da78012e190e41a
Zuhaib Akhtar <zuhaibakhtarkhan@gmail.com>
2025-11-09 23:43:27 +0500

    Added 1399A.cpp

--- 1399A.cpp
+++ 1399A.cpp
@@ -6,8 +6,23 @@ int main() {
     cin >> t;
 
     while(t--) {
+        int n, i, j;
+        cin >> n;
+    
+        vector <int> v(n);
+        bool c = false;
+        for(int &x: v) cin >> x;
+        sort(v.begin(), v.end());
+        if(n != 1) {
+            for(int i = 0; i < n -1; i++) {
+                if(abs(v[i] - v[i+1]) > 1) {
+                    c = true;
+                    break;
+                }
+            }
+        }
         
-
-        
+        (c) ? (cout << "NO\n") : (cout << "YES\n");
+              
     }
 }
