0e28f4dc16a687a950914aaa936ebf312feddf6f
Zuhaib Akhtar <zuhaibakhtarkhan@gmail.com>
2025-11-09 23:43:42 +0500

    Added 9A.cpp

--- 9A.cpp
+++ 9A.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main() {
     int y, w;
     cin >> y >> w;
-    // int m = max(y, w), o = 6 - m + 1;
-    // if(o%2 == 0) cout << o/2 << "/" << 3 << "\n";
-    // else if(o%3 == 0) cout << o/3 << "/" << 2 << "\n";
-    // else if(o== 6) cout << "1/1\n";
-    // else if(o == 5) cout << "5/6\n";
-    // else cout << "0/1\n"; 
+
+    int s = 6 - ((y > w) ? y : w) + 1;
+
+    int g = gcd(s, 6);
+
+    cout << s/g << "/" << 6/g;
 }
