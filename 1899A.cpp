8ecade47c83bd59cc1476950f9843dbb2971558a
Zuhaib Akhtar <zuhaibakhtarkhan@gmail.com>
2025-11-11 23:00:47 +0500

    Added 1899A.cpp

--- 1899A.cpp
+++ 1899A.cpp
@@ -1,4 +1,4 @@
-#include <bits/sdtc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
@@ -8,6 +8,11 @@ int main() {
     while(t--) {
         int n;
         cin >> n;
-                
+
+        if(n%3 != 0) {
+            cout << "First\n";
+        } else cout << "Second\n";
+        
+
     }
 }
