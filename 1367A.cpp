0aab6b612e056d5cd2fe44dd896fae3261652b23
Zuhaib Akhtar <zuhaibakhtarkhan@gmail.com>
2025-11-12 23:39:58 +0500

    Added 1367A.cpp

--- 1367A.cpp
+++ 1367A.cpp
@@ -8,6 +8,13 @@ int main() {
         string b;
         cin >> b;
 
-        
+        int l = b.length();
+
+        cout << b[0];
+        for(int i = 1; i< l; i += 2) {
+            cout << b[i];
+        }
+
+        cout << "\n";
     }
 }
