7cf6d84213c3c717da00e9a2368dd0ede056c9e9
Zuhaib Akhtar <zuhaibakhtarkhan@gmail.com>
2025-11-15 23:11:14 +0500

    Added 318A.cpp

--- 318A.cpp
+++ 318A.cpp
@@ -2,8 +2,15 @@
 using namespace std;
 
 int main() {
-    long long n, k;
+    long long n, k, con;
     cin >> n >> k;
 
-    
+    if(n%2 == 0) con = n/2;
+    else con = n/2 + 1;
+
+    if(k <= con) {
+        cout << 1 + (k - 1)*2;
+    } else {
+        cout << 2 + ((k - con) - 1)*2;
+    }
 }
